3e305d8bd92e7e4857ec97cb540fc74be3144409
developerfidaur <developerfidaur@gmail.com>
2025-03-11 15:21:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.querySelector('#hamburger').addEventListener('click',()=>{
   document.querySelector('.mobileMenu').classList.toggle('menuInvisible')
 })
+ 
+   function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
+ 
